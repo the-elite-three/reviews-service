43ecfbf57358478100ce90f807e67abb9a9ad666
rachelb1239 <rachel@Rachels-MacBook-Pro.local>
2019-09-12 17:36:58 -0600

    add faker, optimized write

--- db/generatedata.js
+++ db/generatedata.js
@@ -1,19 +1,28 @@
 const fs = require('fs');
+const faker = require('faker');
 
-const tempHeaders = ['id', 'review_id', 'url'];
+const tempConfig = {
+  headers: ['id', 'review_id', 'url'],
+  getFakeData: () => faker.fake('{{random.number}},{{image.imageUrl}}'),
+};
 
-const generateFile = (headers) => {
+const generateFile = (config) => {
+  const { headers, getFakeData } = config;
   const writeStream = fs.createWriteStream('test.csv');
   writeStream.write(`${headers.join()}\n`);
-  const tempData = [];
-  for (let i = 0; i < 10; i += 1) {
-    tempData.push(i);
+  let counter = 0;
+  for (let i = 0; i < 100000; i += 1) {
+    let fileBuffer = '';
+    for (let j = 0; j < 100; j += 1) {
+      counter += 1;
+      fileBuffer += `${counter},${getFakeData()}\n`;
+    }
+    writeStream.write(fileBuffer);
   }
-  writeStream.write(tempData.join());
   writeStream.on('finish', () => {
     console.log('done');
   });
   writeStream.end();
 };
 
-generateFile(tempHeaders);
+generateFile(tempConfig);
