c970dd2a65cb05d07f7840d5be645c5cf0c98e61
rachelb1239 <rachel@Rachels-MacBook-Pro.local>
2019-09-16 12:12:27 -0600

    add all routes

--- db/queries.js
+++ db/queries.js
@@ -3,13 +3,21 @@ const { pool } = require('./pool');
 const getReviews = (req, res) => {
   const { productid } = req.params;
   pool.query(`SELECT * FROM review WHERE product_id = ${productid}`, (err, results) => {
-    if (err) {
-      console.log(err);
-    }
+    if (err) res.status(500).json(err);
     res.status(200).json(results.rows);
   });
 };
 
+const getReviewMeta = (req, res) => {
+  const { productid } = req.params;
+  pool.query(`SELECT * FROM review WHERE product_id = ${productid}`, (err, results) => {
+    if (err) res.status(500).json(err);
+    res.status(200).json(results.rows);
+  });
+};
+
+
 module.exports = {
   getReviews,
+  getReviewMeta,
 };
--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,27 @@
+/*
+  Routes for reviews endpoint
+
+  GET /reviews/:product_id/list
+  GET /reviews/:product_id/meta
+  POST /reviews/:product_id
+  PUT /reviews/helpful/:review_id
+  PUT /reviews/report/:review_id
+*/
+
 const express = require('express');
 const db = require('../db/queries');
 
 const router = express.Router();
 
+// Get routes
 router.get('/:productid/list', db.getReviews);
+router.get('/:productid/meta', db.getReviews);
+
+// Post routes
+router.post('/:productid', db.getReviews);
+
+// Put routes
+router.put('/helpful/:review_id', db.getReviews);
+router.put('/report/:review_id', db.getReviews);
 
 module.exports = router;
